c672450a011e2b9c1de37b56bcfe1ad1caa23843
Petek İrem HIZLI <162015902+petekiremhizli@users.noreply.github.com>
2024-05-16 23:52:42 +0300

    Web-Projesi
    Hakkımda sayfasında değişiklikler yaptım. Giriş ve iletişim sayfasını yapmaya başladım.

---
+++ WebProje/js/ilgialanim/kopek.js
@@ -0,0 +1,38 @@
+function getBreeds() {
+    fetch('https://api.thedogapi.com/v1/breeds')
+        .then(response => response.json())
+        .then(data => {
+            var select = document.getElementById("breeds");
+            data.forEach(breed => {
+                var option = document.createElement("option");
+                option.text = breed.name;
+                option.value = breed.id;
+                select.add(option);
+            });
+        })
+        .catch(error => {
+            console.error('Hata:', error);
+        });
+}
+
+function getBreedInfo() {
+    var breedId = document.getElementById("breeds").value;
+    if (!breedId) {
+        document.getElementById("breedInfo").innerHTML = "";
+        return;
+    }
+
+    fetch(`https://api.thedogapi.com/v1/images/search?breed_id=${breedId}`)
+        .then(response => response.json())
+        .then(data => {
+            var breedInfoDiv = document.getElementById("breedInfo");
+            var imageUrl = data[0].url;
+            breedInfoDiv.innerHTML = `<img src="${imageUrl}" alt="Köpek Resmi">`;
+        })
+        .catch(error => {
+            console.error('Hata:', error);
+        });
+}
+
+// Sayfa yüklendiğinde köpek ırklarını al
+window.onload = getBreeds;
