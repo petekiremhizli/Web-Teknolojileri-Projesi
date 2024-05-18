eba90049853c24b8c91d64bd59d32ebcad09d01e
Petek İrem HIZLI <162015902+petekiremhizli@users.noreply.github.com>
2024-05-18 23:59:58 +0300

    web - proje
    .

--- WebProje/php/giris.php
+++ WebProje/php/giris.php
@@ -1,37 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Giriş</title>
-   
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"  crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="css/giris.css">
-</head>
-<body>
-
-
-<div class="container text-center">
-
-
-  <div class="row">
-    <div class="col">
-    <?php 
-    if ($_POST["e-nail"] == "G231210030@sakarya.edu.tr" && $_POST["password"] == "G231210030") {
+<?php 
+    if ($_POST["e-mail"] == "G231210030@sakarya.edu.tr" && $_POST["password"] == "G231210030") {
          echo "Hoşgeldin " . $_POST["name"];
+         exit();
                 
     } else {
                    
         echo "Şifre ya da e-mail hatalı";
-        header("refresh:2; giris.html");
+        header("refresh:10; URL=giris.html");
+        exit();
                 }
-     ?>
-    </div>
-   
-   
-  </div>
-</div>
-</body>
-</html>
+ ?>
